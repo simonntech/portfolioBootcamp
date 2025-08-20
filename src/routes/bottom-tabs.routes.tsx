2738d6bc0c5d029bcb386d2823d1ad347e95a893
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-19 21:02:25 -0300

    bottom tabs refactor

--- src/routes/bottom-tabs.routes.tsx
+++ src/routes/bottom-tabs.routes.tsx
@@ -3,28 +3,60 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import SkillsScreen from "../screens/SkillsScreen";
 import { BlurView } from "expo-blur";
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const {Navigator, Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export function BottomTabsRoutes() {
     return (
         <Navigator
             screenOptions={{
-                headerShown:false,
+                headerShown: false,
                 headerStatusBarHeight: 50,
-                tabBarStyle: {position: 'absolute'},
+                tabBarStyle: { position: 'absolute' },
                 tabBarBackground: () => (
-                    <BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill} />
+                    <BlurView tint="light" intensity={40} style={StyleSheet.absoluteFill} />
                 ),
+                tabBarLabelStyle: {
+                    height: 150
+                }
             }}
         >
-            <Screen 
+            <Screen
                 name="home"
                 component={HomeScreen}
+                options={{
+                    title: 'Minhas Redes',
+                    tabBarLabelStyle: {
+                        fontSize: 16,
+                        color: 'white'
+                    },
+                    tabBarIcon: () => (
+                        <MaterialCommunityIcons
+                            name="link"
+                            color={'white'}
+                            size={25}
+                        />
+                    )
+                }}
             />
-            <Screen 
+            <Screen
                 name="skills"
                 component={SkillsScreen}
+                options={{
+                    title: 'Minhas Habilidades',
+                    tabBarLabelStyle: {
+                        fontSize: 16,
+                        color: 'white'
+                    },
+                    tabBarIcon: () => (
+                        <MaterialCommunityIcons
+                            name="rocket-launch"
+                            color={'white'}
+                            size={25}
+                        />
+                    )
+                }}
             />
         </Navigator>
     )
